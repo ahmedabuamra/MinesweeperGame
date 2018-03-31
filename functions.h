fce6d9677a87d401f4322cd9a0d91f1f28b2931c
Ahmed Elmayyah <a.elmayyah@gmail.com>
2018-04-01 00:01:18 +0200

    Update #4.1 (CLI Overhaul)
    This update included:
    1-Displaying the time spent to finish a game.
    
    2-Added 4 difficulties, and an option for a custom size.
    
    3-The following functions have been edited:
    i-getInitialValues -> initialiseGame
    ii- printVisible
    
    4-The function chooseDifficulty has been added.
    
    5-General improvement to the CLI.
    
    6-Added the option to win by opening all of the tiles that don't contain the mines.

--- functions.h
+++ functions.h
@@ -6,6 +6,7 @@
 #include <set>
 #include <cstring>
 #include <iomanip>
+#include <chrono> //Includes the time related functions implemented in C++11
 
 #define endl '\n'
 #define Mines_Flag 9 //This flag is set to 9 as it is impossible for one tile to have more than 8 mines in its adjacent tiles.
@@ -18,9 +19,16 @@ using std::set;
 using std::pair;
 using std::setw;
 
+//For time related functions from the chrono file header
+using std::chrono::steady_clock;
+using std::chrono::minutes;
+using std::chrono::seconds;
+using std::chrono::duration_cast;
+
 //Check the function definitions in the functions.cpp file to see the explanation for what each function does
 void initVisibleToHash(int height, int width);
-void getInitialValues(int &height, int &width, int &numOfMines);
+void chooseDifficulty(char c, int &height, int &width, int &numOfMines);
+void initialiseGame(int &height, int &width, int &numOfMines);
 void randomiseMineCoordinates(set < pair<int, int> > &minesCoordinates,
 										int numOfMines, int height, int width);
 void setValueForTile(int r, int c);
