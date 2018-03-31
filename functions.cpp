c5d46d8155322fd5211bb06180cb306e3e32cd75
Ahmed Elmayyah <a.elmayyah@gmail.com>
2018-04-01 00:00:37 +0200

    Update #4.1 (CLI Overhaul)
    This update included:
    1-Displaying the time spent to finish a game.
    
    2-Added 4 difficulties, and an option for a custom size.
    
    3-The following functions have been edited:
    i-getInitialValues -> initialiseGame
    ii- printVisible
    
    4-The function chooseDifficulty has been added.
    
    5-General improvement to the CLI.
    
    6-Added the option to win by opening all of the tiles that don't contain the mines.

--- functions.cpp
+++ functions.cpp
@@ -8,21 +8,55 @@ bool clickedOnMine(false);
 int grid[50][50];
 char visible[50][50];
 
+
+
 void initVisibleToHash(int height, int width)
 {
+	//A function that initialises that visible grid to the character '#'
 	for (int i = 1; i <= height; i++)
 		for (int j = 1; j <= width; j++)
 			visible[i][j] = '#';
 }
 
-void getInitialValues(int &height, int &width, int &numOfMines)
+void chooseDifficulty(char c, int &height, int &width, int &numOfMines)
+{
+	//A function that initialises the grid size and number of mines based on the difficulty
+	//chosen by the player.
+	switch (c)
+	{
+		case 'E':height = 8, width = 8, numOfMines = 10; break; //Easy case
+
+		case 'M':height = 10, width = 10, numOfMines = 20; break; //Medium case
+
+		case 'H':height = 15, width = 15, numOfMines = 45; break; //Hard case
+
+		case 'B':height = 20, width = 20, numOfMines = 80; break; //Brutal case
+
+		case 'C':
+		{
+			cout << "Enter grid size (height * width), and # of mines: ";
+			cin >> height >> width >> numOfMines;
+			break;
+		}  //Custom case
+		default: cout << "Error!" << endl;
+	}
+
+
+}
+
+void initialiseGame(int &height, int &width, int &numOfMines)
 {
 	//A function that passes the height, width and the number of mines in the grid
 	//by reference to input them
 
 	cout << setw(15) << "Welcome to Minesweeper!" << endl;
-	cout << setw(0) << "Enter grid size (height * width), and # of mines: ";
-	cin >> height >> width >> numOfMines;
+	cout << setw(0) << "Enter the difficulty you want to play (E, M, H, B, C): ";
+
+	char diff;
+	cin >> diff;
+
+	chooseDifficulty(diff, height, width, numOfMines);
+
 }
 
 void randomiseMineCoordinates(set < pair<int, int> > &minesCoordinates,
@@ -88,14 +122,19 @@ void printVisible(int height, int width)
 	//A function that prints the visible grid to the user
 
 	//Printing out the top guide
-	cout << "  ";
+	cout << "    ";
 	for (int i = 1; i <= width; i++)
 		cout << (char) (i + 64) << ' ';
+	cout << endl << "    ";
+
+	//Printing a separator between the top guide and the grid.
+	for (int i = 1; i <= width; i++)
+		cout << "- ";
 	cout << endl;
 
 	for (int i = 1; i <= height; i++)
 	{
-		cout << (char) (i + 64) << ' '; // Printing out a character from the side guide
+		cout << (char) (i + 64) << " | "; // Printing out a character and a pipe from the side guide
 		//before printing the row itself
 		for (int j = 1; j <= width; j++)
 		{
@@ -103,29 +142,6 @@ void printVisible(int height, int width)
 		}
 		cout << endl;
 	}
-}
-
-void printActual(int height, int width)
-{
-	//A function that prints the actual grid to the user, used for testing only
-	//It will be removed in the final release
-	for (int row = 1; row <= height; row++)
-	{
-		cout << "|";
-		for (int col = 1; col <= width; col++)
-		{
-			if (grid[row][col] == Mines_Flag)
-				cout << "+|";
-
-			else if (grid[row][col] == 0)
-				cout << "_|";
-
-			else
-				cout << grid[row][col] << '|';
-		}
-		cout << endl;
-	}
-
 }
 
 void openTiles(int y, int x, int height, int width)
@@ -156,6 +172,8 @@ void openTiles(int y, int x, int height, int width)
 
 void clickTile(int height, int width)
 {
+	// A function that imitates the action of clicking on a tile.
+
 	char yC, xC;
 	char operation;
 
@@ -203,7 +221,10 @@ void clickTile(int height, int width)
 
 char endGame(int height, int width, int numOfMines)
 {
+	// A function that checks if the game has ended or not by checking the visible grid.
+
 	int cntr(0), flagCntr(0);
+	bool emptyAreVisible(true);
 
 	for (int i = 1; i <= height; i++)
 		for (int j = 1; j <= width; j++)
@@ -214,12 +235,21 @@ char endGame(int height, int width, int numOfMines)
 				flagCntr++;
 		}
 
+	for (int i = 1; i <= height; i++)
+		for (int j = 1; j <= width; j++)
+		{
+			if (grid[i][j] != Mines_Flag && visible[i][j] == '#')
+				emptyAreVisible = false;
+		}
+
+
 	cout << "Number of flags placed:" << flagCntr << endl;
-	if (cntr == numOfMines && flagCntr == numOfMines)
+	if (cntr == numOfMines && flagCntr == numOfMines || emptyAreVisible)
 	{
 		cout << "Congratulations, you win!" << endl;
 		return 'W'; // Returns W (for win) if the user puts flags on all of the mines
 	}
+
 
 	if (clickedOnMine)
 	{
@@ -233,11 +263,25 @@ char endGame(int height, int width, int numOfMines)
 
 void playGame(int height, int width, int numOfMines)
 {
+	auto start = steady_clock::now(); //Sets a start for counting time.
+
 	cout << "Enter tile address and then the command(?,F,O): ";
+
 	//Playing the game!
 	do
 	{
 		clickTile(height, width);
 	}
 	while (endGame(height, width, numOfMines) == 'N');
+
+	auto end = steady_clock::now(); //Sets an end for counting time.
+
+	auto tSeconds = duration_cast <seconds> (end - start).count(); //Counting seconds
+	auto tMinutes = duration_cast <minutes> (end - start).count(); //Counting minutes
+
+	if (tMinutes > 0) //Condition that makes sure to prrint the minutes only if they are larger than 0.
+	cout << "The game took " << tMinutes << " minutes, and "
+		  << tSeconds - (tMinutes*60) << " seconds." << endl;
+	else
+		cout << "The game took " << tSeconds << " seconds." << endl;
 }
