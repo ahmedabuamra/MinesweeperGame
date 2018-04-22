9d61b6144969d8ffa099e8449a01c4f584eb783c
ahmedabuamra <ah.abuamra@gmail.com>
2018-04-22 15:53:17 -0700

    Update #5.1.1
    Access visible from main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@ int main()
 	//Declaration of variables
 	set < pair<int, int> > minesCoordinates;
 	int numOfMines, width, height;
-
+	char but[50][50];
 	vector <player> topPlayers; // Vector to hold the top players.
 	player mainPlayer = {}; //Struct to hold the data for the player.
 
-	//Linking srand with time to achieve less limited randomness
+							//Linking srand with time to achieve less limited randomness
 	srand(time(NULL));
 
 	//Input
@@ -23,17 +23,17 @@ int main()
 	//Placing the mines
 	placeMines(minesCoordinates);
 
-
 	//Setting the numerical values for the tiles on the actual grid, indicating the number of mines adjacent to each tile.
 	setValuesForGrid(height, width);
 
 	//Printing out the visible grid initially to the user
 	printVisible(height, width);
 
+	//every time you need visible from functions.cpp
+	getVisiable(but);
 	//Playing the game!
-	playGame(height, width, numOfMines, mainPlayer, topPlayers);
-
-    return 0;
+	//playGame(height, width, numOfMines, mainPlayer, topPlayers);
+	return 0;
 }
 
 
