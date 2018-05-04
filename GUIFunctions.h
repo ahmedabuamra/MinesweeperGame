780f769f54e0e1818577e8dd39b4fc862266fb9e
ahmedabuamra <ah.abuamra@gmail.com>
2018-05-05 01:12:56 +0200

    Update GUIFunctions.h

--- GUIFunctions.h
+++ GUIFunctions.h
@@ -4,7 +4,7 @@
 #include"functions.h"
 
 //GUI functions
-void MainWindowDisplay(float W_width, float W_height, string W_name);
+void MainWindowDisplay(float W_width, float W_height, string W_name, tgui::Button::Ptr** gridButtons);
 void MainMenu();
 void LevelMenu();
 void Level_Select(int level);
@@ -12,7 +12,7 @@ void Launch_Level(int width, int height, int numOfmines);
 void DisableWidgets();
 void CustomLevel();
 void Back_Button();
-void SoundPlay(string type , string path,bool isLoop);
-void Store(int i, int j, int width, int height, char operation);
-void FinishedLevel(int width , int height , bool won);
+void SoundPlay(string type, string path, bool isLoop);
+void Store(int i, int j, int width, int height, char operation, tgui::Button::Ptr** gridButtons);
+void FinishedLevel(int width, int height, bool won, tgui::Button::Ptr** gridButtons);
 void Credits();
