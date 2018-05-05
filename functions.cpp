94ccb0c15dbcd71c9cc57484e47eee6f95e01344
Ahmed Elmayyah <a.elmayyah@gmail.com>
2018-05-05 16:59:05 +0200

    Update G#1.1
    Fixed issue #20.

--- functions.cpp
+++ functions.cpp
@@ -417,13 +417,13 @@ char endGame(int height, int width, int numOfMines)
 	for (int i = 1; i <= height; i++)
 		for (int j = 1; j <= width; j++)
 		{
-			if (grid[i][j] != Mines_Flag && visible[i][j] == '#')
+			if ((grid[i][j] != Mines_Flag && visible[i][j] == '#' ) && visible[i][j] != 'F' )
 				emptyAreVisible = false;
 		}
 
 
 	cout << "Number of flags placed:" << flagCntr << endl;
-	if (cntr == numOfMines && flagCntr == numOfMines || emptyAreVisible)
+	if ((cntr == numOfMines && flagCntr == numOfMines) || emptyAreVisible)
 	{
 		cout << "Congratulations, you win!" << endl;
 		won = true;
--- main.cpp
+++ main.cpp
@@ -18,15 +18,18 @@ https://tgui.eu/									*
 int main()
 {
 	SoundPlay("music", "sounds/Music.flac", true);
+
 	try
 	{
 		MainMenu();
 	}
+
 	catch (const tgui::Exception& e)
 	{
 		cout << "Failed to load TGUI widgets: " << e.what() << endl;
 		return 1;
 	}
+
 	MainWindowDisplay(1024, 768, "Minesweeper");
 	return 0;
 }
